e40063f877d4a9c29ea40ced98724e58f04ecf29
wmj <1478147296@qq.com>
2022-08-07 17:00:33 +0800

    Initial commit

---
+++ src/main/java/com/wmj/hesuan/HesuanApplication.java
@@ -0,0 +1,13 @@
+package com.wmj.hesuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HesuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HesuanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wmj/hesuan/HesuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmj.hesuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HesuanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
